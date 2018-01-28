42dc31bb645f5eeca31e521bbc43af5a66b78d79
rio197 <rio197@Kedaung-PC>
2018-01-28 12:18:55 -0500

    Add main

---
+++ src/alog/binarysearch/App.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alog.binarysearch;
+
+/**
+ *
+ * @author rio197
+ */
+public class App {
+    public static void main(String[] args) {
+        int[] myArray = new int[10]; 
+        int jj = 11;
+                
+        for (int ii=0; ii < myArray.length; ii++) {
+            myArray[ii] = jj;
+            System.out.print(myArray[ii]);
+            jj++;
+        }
+    }
+}
